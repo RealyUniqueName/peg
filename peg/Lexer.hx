bf0916285a47eb7a8dc615ba0c60639b950b6d07
Aleksandr Kuzmenko <alex@stablex.ru>
2019-05-25 00:45:55 +0300

    Fixed reading stdout of php

--- peg/Lexer.hx
+++ peg/Lexer.hx
@@ -1,10 +1,12 @@
 package peg;
 
-import haxe.Resource;
+import haxe.io.Eof;
+import haxe.io.Input;
+import haxe.Exception;
+import haxe.io.Error;
+import haxe.io.BytesBuffer;
 import haxe.Json;
 import sys.io.Process;
-
-using StringTools;
 
 class Lexer {
 	public final tokens:ReadOnlyArray<Token>;
@@ -28,12 +30,29 @@ class Lexer {
 	 * Execute php interpreter with given arguments
 	 */
 	static function php(args:Array<String>):{stdout:String, stderr:String, exitCode:Int} {
+		inline function read(io:Input, buf:BytesBuffer) {
+
+		}
+
 		var p = new Process('/home/alex/.phpenv/shims/php', args);
+		var stdout = new BytesBuffer();
+		while(true) {
+			try {
+				stdout.addByte(p.stdout.readByte());
+			} catch(e:Error) {
+				switch e {
+					case Blocked:
+					case e: throw Exception.wrapWithStack(e);
+				}
+			} catch(e:Eof) {
+				break;
+			}
+		}
 		var exitCode = p.exitCode(true);
 		var result = {
 			exitCode: exitCode.sure(),
 			stderr: p.stderr.readAll().toString(),
-			stdout: p.stdout.readAll().toString()
+			stdout: stdout.getBytes().toString()
 		}
 		p.close();
 		return result;
