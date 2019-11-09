e26f0234fd0d79edc4a0ee2f91cb52bfe1b332d4
Daniel Llewellyn <daniel@bowlhat.net>
2019-11-09 01:14:51 +0000

    Add @:native names
    Signed-off-by: Daniel Llewellyn <daniel@bowlhat.net>

--- Run.hx
+++ Run.hx
@@ -38,9 +38,15 @@ class Run {
 		return '${v.visibility} var ${getVarName(v.name)}:${getType(v.type)}';
 	}
 
-	static function getFunction(fn:peg.php.PFunction, forceStatic:Bool = false):String {
+	static function getFunction(fn:peg.php.PFunction, isNamespaceGlobal:Bool = false, ?namespace:String):String {
 		var args = fn.args.map(arg -> '${getVarName(arg.name)}:${getType(arg.type)}').join(', ');
-		return '${fn.isAbstract ? 'abstract ' : ''}${fn.isFinal ? 'final ' : ''}${fn.visibility}${forceStatic || fn.isStatic ? ' static' : ''} function ${fn.name}(${args}):${getType(fn.returnType)};';
+		var inlineCallParams = fn.args.map(arg -> getVarName(arg.name)).join(', ');
+		var callSite = '';
+		if (isNamespaceGlobal) {
+			var ns = namespace != '' ? '\\\\${namespace}\\\\': '';
+			callSite = ' return untyped __call__(\'${ns}${fn.name}\'${inlineCallParams != '' ? ', ' + inlineCallParams : ''})';
+		}
+		return '${fn.isAbstract ? 'abstract ' : ''}${fn.isFinal ? 'final ' : ''}${fn.visibility}${isNamespaceGlobal || fn.isStatic ? ' static inline' : ''} function ${fn.name}(${args}):${getType(fn.returnType)}${callSite};';
 	}
 
 	static function usage() {
@@ -98,45 +104,61 @@ class Run {
 		}
 
 		if (combinedNamespaces != null) {
-			for (pkg => namespace in combinedNamespaces) {
-				if (pkg != '') {
-					pkg = ~/\\/g.replace(pkg.toLowerCase(), '.');
-					Sys.println('package ${basePackage}.${pkg};');
+			var slashesRE = ~/\\/g;
+			var leadingDotRE = ~/^\./;
+
+			for (nsName => namespace in combinedNamespaces) {
+				if (nsName != '') {
+					var p = slashesRE.replace(nsName.toLowerCase(), '.');
+					Sys.println('package ${basePackage}.${p};');
 				} else {
 					Sys.println('package ${basePackage};');
 				}
 
 				if (namespace.constants.length > 0 || namespace.functions.length > 0) {
-					Sys.println('extern class ${basePackage} {');
+					Sys.println('extern class GLOBALS {');
 					for (c in namespace.constants) {
-						Sys.println('    ${getConst(c)}');
+						Sys.println('    ' + getConst(c));
 					}
 					for (fn in namespace.functions) {
-						Sys.println('    ${getFunction(fn, true)}');
+						Sys.println('    ' + getFunction(fn, true, namespace.name));
 					}
 					Sys.println('}');
 				}
 
 				for (cls in namespace.classes) {
 					var kwd = cls.isInterface ? 'interface' : 'class';
-					var parent = ~/^_/.replace(~/\\/g.replace(cls.parent, '_'), '');
-					var ext = cls.parent != null && cls.parent != '' ? 'extends ${pkg != '' ? basePackage + '.' : ''}${parent}' : '';
+					var phpNS = slashesRE.replace(nsName, '\\\\\\\\');
+					if (phpNS != '') {
+						phpNS += '\\\\';
+					}
+					var parent = leadingDotRE.replace(slashesRE.replace(cls.parent, '.'), '');
+
+					var clsExtends = '';
+					if (cls.parent != null && cls.parent != '') {
+						clsExtends = 'extends ${basePackage}.';
+						if (namespace.name == '') {
+							clsExtends += 'GLOBALS.';
+					 	}
+						clsExtends += parent;
+					}
 					var imp = cls.interfaces.length > 0 ? 'implements ${cls.interfaces.join(', ')}' : '';
 
-					Sys.println('extern $kwd ${cls.name} ${ext} ${imp} {');
+					Sys.println('@:native(\'\\\\' + phpNS + cls.name + '\')');
+					Sys.println('extern ${kwd} ${cls.name} ${clsExtends} ${imp} {');
 					for (c in cls.constants) {
 						if (c.visibility != VPrivate) {
-							Sys.println('    ${getConst(c)}');
+							Sys.println('    ' + getConst(c));
 						}
 					}
 					for (v in cls.vars) {
 						if (v.visibility != VPrivate) {
-							Sys.println('    ${getVar(v)}');
+							Sys.println('    ' + getVar(v));
 						}
 					}
 					for (fn in cls.functions) {
 						if (fn.visibility != VPrivate) {
-							Sys.println('    ${getFunction(fn)}');
+							Sys.println('    ' + getFunction(fn));
 						}
 					}
 					Sys.println('}');
