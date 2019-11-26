1caf535bd1367a14e8f52710bae86b016e2acfab
Aleksandr Kuzmenko <alex@stablex.ru>
2019-11-26 22:10:21 +0300

    validate sample externs

--- test/src/TestAll.hx
+++ test/src/TestAll.hx
@@ -1,48 +1,62 @@
-import peg.Generator;
-import peg.PegException;
+using haxe.io.Path;
+using sys.FileSystem;
+using sys.io.File;
+using StringTools;
 
 class TestAll {
-	// static public function main() {
-	// 	trace(Sys.args());
-	// }
+	static public function main() {
+		var pegPhp = 'bin/php/index.php';
+		var phpDir = 'test/data/php';
+		var outDir = 'bin/generated';
+		var expectedDir = 'test/data/expected';
+		Sys.println('Generating externs of the test data from $phpDir...');
+		var exitCode = Sys.command('php', [pegPhp, '--php', phpDir, '--out', outDir]);
+		if(exitCode != 0) {
+			Sys.stderr().writeString('Failed to generate externs.\n');
+			Sys.exit(exitCode);
+		}
 
-	static function main() {
-		var cnt = 0;
-		Sys.print('');
-		var allNamespaces = [];
-		for (file in new peg.SourcesIterator('vendor/zendframework/zend-json')) {
-			Sys.print('\rParsing php files: ${++cnt}');
-
-			var namespaces = try {
-				file.parse();
-			} catch(e:UnexpectedTokenException) {
-				Sys.println('\n${file.path}:${e.token.line} FAIL >>');
-				Sys.println(e.toString());
-				Sys.exit(1);
-				return;
-			} catch(e:ParserException) {
-				Sys.println('\n${file.path} FAIL >>');
-				Sys.println(e.toString());
-				Sys.exit(1);
-				return;
-			} catch(e:PegException) {
-				Sys.println('\n${file.path} FAIL >>');
-				Sys.println(e.toString());
-				Sys.println('<< FAIL, skipped');
-				continue;
-			}
-			for (namespace in namespaces) {
-				allNamespaces.push(namespace);
+		Sys.println('Validating generated externs against expected result from $expectedDir...');
+		var success = true;
+		for(relative => absolute in findHxFiles(expectedDir)) {
+			var expectedContent = absolute.getContent();
+			var actualPath = Path.join([outDir, relative]);
+			var actualContent = actualPath.getContent();
+			if(expectedContent != actualContent) {
+				success = false;
+				Sys.stderr().writeString('Generated content does not match expected content for file $relative\n');
+				Sys.stderr().flush();
+				Sys.command('git', ['diff', '--no-index', absolute, actualPath]);
 			}
 		}
-		Sys.println('');
-		cnt = 0;
-		var gen = new Generator(
-			allNamespaces,
-			'bin/externs',
-			()-> Sys.print('\rWriting Haxe externs: ${++cnt}'),
-			() -> Sys.println('\nAll done!')
-		);
-		gen.run();
+		if(!success) {
+			Sys.exit(1);
+		}
+	}
+
+	/**
+	 * Returns a list of hx files with paths relative to `root`.
+	 * If `root` is not provided then all the found file paths will be relative to `path`.
+	 * Returns a map of [relative_to_root => absolute_path ]
+	 */
+	static function findHxFiles(path:String, ?root:String, ?result:Map<String,String>):Map<String,String> {
+		var result = switch result {
+			case null: new Map();
+			case r: r;
+		}
+		var root = switch root {
+			case null: path.fullPath().addTrailingSlash();
+			case r: r;
+		}
+
+		path = path.fullPath();
+		if(path.isDirectory()) {
+			for(entry in path.readDirectory()) {
+				findHxFiles(Path.join([path, entry]), root, result);
+			}
+		} else if(path.toLowerCase().endsWith('.hx')) {
+			result.set(path.replace(root, ''), path);
+		}
+		return result;
 	}
 }
